0f408347730f4f9286b231434bf6815b2c5055fc
JRProgramming <ramirezjohnny@icloud.com>
2017-07-20 19:03:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 for(let i = 1; i <= 3; i++) {
   for(let j = 1; j <= 3; j++) {
     document.getElementById('r' + i + 'c' + j).onclick = function() {
+      if(this.innerHTML = "")
+      {
       this.innerHTML = 'X';
+      }
+      else
+      {
+      this.innerHTML = 'O';
+      }
     }
   }
 }
