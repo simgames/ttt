9e46a7593a111ef300c9a4ded7e0a86dcddf5bd4
Allen B <pingguo.ny+git@gmail.com>
2017-07-20 14:19:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-window.addEventListener("load", function() {
-  const canvas = document.getElementById("board");
-  const ctx = canvas.getContext('2d');
-  
-  ctx.fillStyle = 'green';
-  ctx.fillRect(10, 10, 100, 100);
-});
+
