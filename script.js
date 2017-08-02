3b83f10270ab5adce602efea55b2cede2b0a51d5
JRProgramming <ramirezjohnny@icloud.com>
2017-08-01 20:23:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -133,15 +133,15 @@ function computerTurn()
 
 }
 var r = 1
-function checkWinner(r)
-{
+function checkWinner()
+{  
     var letterxr1 = 0;
     var letteror1 = 0;
     var letterxc1 = 0;
     var letteroc1 = 0;
     for (i = 1; i < 4; i++)
     {
-        console.log(r);
+       
         if (document.getElementById("r" + i + "c" + r).innerHTML == "X" || document.getElementById("r" + i + "c" + r).innerHTML == "O")
         {
             if (document.getElementById("r" + i + "c" + r).innerHTML == "X")
